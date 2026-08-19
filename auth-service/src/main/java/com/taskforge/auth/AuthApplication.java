94e514a949de917ba1f76d8529d1b017867ed8bd
dipto-kainin <kaininhop@gmail.com>
2026-08-19 16:50:56 +0530

    fix: exclude UserDetailsServiceAutoConfiguration in AuthApplication to prevent default security conflicts

--- auth-service/src/main/java/com/taskforge/auth/AuthApplication.java
+++ auth-service/src/main/java/com/taskforge/auth/AuthApplication.java
@@ -3,6 +3,7 @@ package com.taskforge.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class, JmxAutoConfiguration.class})
 public class AuthApplication {
